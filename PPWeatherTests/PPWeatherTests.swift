8ea26bc1b631be6460024d881e486fc9b51b8eff
mettmoon <ys_qwerty700@naver.com>
2014-11-13 12:41:16 +0900

    Initial Commit

---
+++ PPWeatherTests/PPWeatherTests.swift
@@ -0,0 +1,36 @@
+//
+//  PPWeatherTests.swift
+//  PPWeatherTests
+//
+//  Created by Hayden on 2014. 11. 13..
+//  Copyright (c) 2014년 WEJOApps. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PPWeatherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
